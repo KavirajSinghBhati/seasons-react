207d500bd6b777576ecd10eac332b6703621e775
Kaviraj <ksb1910.07@gmail.com>
2021-09-05 10:56:39 +0530

    season project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { latitude: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (pos) => this.setState({ latitude: pos.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.latitude === null ? (
+          this.state.errorMessage === "" ? (
+            <Spinner message="Pl. accept location request" />
+          ) : (
+            <h1>Error: {this.state.errorMessage}</h1>
+          )
+        ) : (
+          <SeasonDisplay lat={this.state.latitude} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+//state can be initialised using either constructor or state obj inside class
+
+/*
+
+
+ */
